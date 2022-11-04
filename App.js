5258a2dd2aa39ff2e11981d5480e67a6d9d70eb6
Elaine Estainer <elaineestainer@gmail.com>
2022-11-04 15:14:51 -0300

    feat: apostila 05 app lembrete secao 2.11
    implementado o TextInput controlado com a variavel de estado e Button adicionar

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [lembrete, setLembrete] = useState('');
+
+  const onChangeText = (text) => {
+    console.log(text);
+    setLembrete(text);
+  };
+
+  const adicionarLembrete = () => {
+    console.log('Adicionado');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          placeholder='Lembrar...'
+          style={styles.lembreteTextInput}
+          onChangeText={onChangeText}
+          //onChange={(e) => onChangeText(e.target.value)}
+          value={lembrete}
+        />
+        <Button title='Adicionar' onPress={adicionarLembrete} />
+        <Text>{lembrete}</Text>
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+  },
+  lembreteTextInput: {
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 1,
+    marginBottom: 4,
+    padding: 12,
   },
 });
