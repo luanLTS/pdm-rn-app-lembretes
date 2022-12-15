f3dac9ba79abb98ec87f38d17ce395ff087ece2a
Luan Teixeira <luanlts@outlook.com>
2022-12-14 21:13:01 -0300

    feat: salva alteracao

--- App.js
+++ App.js
@@ -1,44 +1,44 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [lembrete, setLembrete] = useState('');
+    const [lembrete, setLembrete] = useState("");
 
-  const onChangeText = (text) => {
-    console.log(text);
-    setLembrete(text);
-  };
+    const onChangeText = (text) => {
+        console.log(text);
+        setLembrete(text);
+    };
 
-  const adicionarLembrete = () => {
-    console.log('Adicionado');
-  };
+    const adicionarLembrete = () => {
+        console.log("Adicionado");
+    };
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <TextInput
-          placeholder='Lembrar...'
-          style={styles.lembreteTextInput}
-          onChangeText={onChangeText}
-          //onChange={(e) => onChangeText(e.target.value)}
-          value={lembrete}
-        />
-        <Button title='Adicionar' onPress={adicionarLembrete} />
-        <Text>{lembrete}</Text>
-      </View>
-      <View></View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <TextInput
+                    placeholder="Lembrar..."
+                    style={styles.lembreteTextInput}
+                    onChangeText={onChangeText}
+                    //onChange={(e) => onChangeText(e.target.value)}
+                    value={lembrete}
+                />
+                <Button title="Adicionar" onPress={adicionarLembrete} />
+                <Text>{lembrete}</Text>
+            </View>
+            <View></View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 40,
-  },
-  lembreteTextInput: {
-    borderBottomColor: '#DDD',
-    borderBottomWidth: 1,
-    marginBottom: 4,
-    padding: 12,
-  },
+    container: {
+        padding: 40,
+    },
+    lembreteTextInput: {
+        borderBottomColor: "#DDD",
+        borderBottomWidth: 1,
+        marginBottom: 4,
+        padding: 12,
+    },
 });
